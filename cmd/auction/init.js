5fa8f850a1ffe2797369b6b956bf49b63e057160
Luis Felipe <luis14_felipe@hotmail.com>
2024-10-28 21:10:42 -0300

    projeto rodando

---
+++ cmd/auction/init.js
@@ -0,0 +1,20 @@
+// init.js
+db = db.getSiblingDB('auctions');
+db.users.insertMany([
+    { 
+        _id:"a6deb003-ba12-4dae-801f-e2f75a56de47", 
+        name: "Luis Felipe"
+    },
+]);
+
+db.auctions.insertMany([
+    {
+        _id: "064f9f8a-42f6-4a12-9b03-9620d001aef5",
+        product_name: "IMAC",
+        category: "Eletrônicos",
+        description: "Produto em excelente estado, com pouco uso e garantia.",
+        condition: 1,
+        status: 0,
+        timestamp: Long('1730158227')
+    },
+]);
